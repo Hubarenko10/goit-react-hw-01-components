8b2ed7516cc738938784c0d69b80ca65d6605604
Ivan Hubarenko <103453171+Hubarenko10@users.noreply.github.com>
2022-11-26 17:22:57 +0200

    edits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
 import { Profile } from "./Profile";
 import user from 'user.json';
+
 export const App = () => {
   return  <div>
-    <Profile
+        <Profile
   username={user.username}
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
 />
-  </div> 
-  
+  </div>
+
+
 };
